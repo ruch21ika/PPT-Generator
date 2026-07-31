6b31730d9c7b91db328bb437e2c6ba9ca5e5f90b
ruch21ika <ruch21ika@gmail.com>
2026-07-31 13:29:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,8 +34,8 @@ elif all(ALL_API):
   model=st.sidebar.selectbox("Gemini-Model-Name",
                               options=["gemini-2.5-flash",
                                       "gemini-2.5-flash-lite",
-                                      "gemini-2.5-flash",
-                                      "gemini-2.5-flash-lite"])
+                                      "gemini-3.5-flash",
+                                      "gemini-3.5-flash-lite"])
   )
 else:
   st.sidebar.info("CHECK-API-KEYS")
